e21ca036a4fb1bd9c536d43f114a11632b08425e
Adrian <adrian.hammermeister55@gmail.com>
2022-02-15 12:42:08 +0100

    calculate statistical values
    mean, tau, std, std_mean, d_s, d_z

---
+++ calc_stats.py
@@ -0,0 +1,55 @@
+import scipy.stats as scs
+import numpy as np
+
+''' 
+# returns the tau for the students-t-distribution for an given convidence interval
+# and a given degree of freedom
+# note: the convidence interval is mutual
+#
+# parameters:
+# ci - int : convidence interval
+# df - int : degrees of freedom
+'''
+def return_T(ci, df):
+    return scs.t.ppf((1+ci)/2, df)
+
+'''
+# returns the mean, standard deviation, std of mean of the data
+# for the std we use degrees of freedom: n-1 (ddof=1)
+#
+# parameters:
+# data - np.ndarray : shape (n,)
+#
+# return:
+# mean, std and std_mean in a tupel
+'''
+def calc_Stat(data):
+    mean = np.mean(data)
+    std = np.std(data, ddof=1) 
+    std_mean = std/np.sqrt(len(data))
+    
+    return (mean, std, std_mean)
+
+'''
+# returns random- and systematitical inaccuracy 
+#
+# parameters:
+# a, b - float: variable for systematical inaccuracy (a + b*x)
+# data - np.ndarray : shape (n,)
+#
+# return:
+# d_s, d_z, d_s + d_z as tupel
+# 
+# d_s - float : systematical inaccuracy
+# d_z - float : random inaccuracy
+'''
+def calc_InAcc(a, b, data):
+    (mean, std, std_mean) = calc_Stat(data)
+    d_s = a + b*mean
+    d_z = return_T(0.95, len(data))*std_mean
+    
+    return (d_s, d_z, d_s+d_z)
+    
+    
+
+
