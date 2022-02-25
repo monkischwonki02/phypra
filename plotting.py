6da3d0f8b367df4f60171601b60aae2f0094fafe
Adrian <adrian.hammermeister55@gmail.com>
2022-02-25 18:02:27 +0100

    plotting
    add errorbarplot for a single graph oder multiple graphs

---
+++ plotting.py
@@ -0,0 +1,59 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+'''
+# creates a plot with errorbars
+#
+# parameters:
+# x,y - np.ndarray (n,) : data
+# err - np.ndarray (n,) : error of y
+# head : title of diagram
+# xlbl : Label of x-Axis
+# ylbl : label of y-Axis
+# cl: color of graph (see on https://matplotlib.org/stable/gallery/color/named_colors.html)
+#
+# return:
+# returns matplotlib.figure.Figure Object 
+'''
+def plot_err(x: np.ndarray,y: np.ndarray,err: np.ndarray, head="", xlbl="", ylbl="", cl=""):
+    fig, ax1 = plt.subplots(1)
+
+    ax1.errorbar(x,y,err, color=cl)
+    ax1.grid()
+    ax1.set(xlabel=str(xlbl), ylabel=str(ylbl))
+    fig.suptitle(str(head))
+    ax1.scatter(x,y,color=cl)
+
+    return fig
+
+
+'''
+# creates a plot with 'm'-Graphs
+# 
+# parameters:
+# x,y  - np.ndarray (n,m) : m lists with n-datapoints
+# err - np.ndarray (n,m) : errors of data
+# head : title of diagram
+# xlbl : Label of x-Axis
+# ylbl : label of y-Axis
+# legend: names of every single graph
+# cl: color of graph (see on https://matplotlib.org/stable/gallery/color/named_colors.html)
+#
+# return:
+# returns matplotlib.figure.Figure Object 
+'''
+def plot_err_mul(x: np.ndarray,y: np.ndarray,err: np.ndarray, head="", xlbl="", ylbl="", legend=[], cl=[]):
+    fig, ax1 = plt.subplots(1)
+    for i in range(x.shape[1]):
+        if len(legend) <= x.shape[1]:
+            for _ in range(x.shape[1] - len(legend)):
+                legend.append("")
+        ax1.errorbar(x[i, :], y[i, :], err[i, :], label=legend[i], c=cl[i])
+        ax1.scatter(x[i,:], y[i,:], c=cl[i])
+
+    ax1.grid()
+    ax1.set(xlabel=str(xlbl), ylabel=str(ylbl))
+    ax1.legend()
+    fig.suptitle(str(head))
+    return fig
+
