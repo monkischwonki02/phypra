b2c420d5a5d777a46c45c1b5254ec0ffd35f5775
Adrian <adrian.hammermeister55@gmail.com>
2022-02-28 21:09:27 +0100

    remove single-plot

--- plotting.py
+++ plotting.py
@@ -1,34 +1,9 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-'''
-# creates a plot with errorbars
-#
-# args:
-# x,y - np.ndarray (n,) : data
-# err - np.ndarray (n,) : error of y
-# head : title of diagram
-# xlbl : Label of x-Axis
-# ylbl : label of y-Axis
-# cl: color of graph (see on https://matplotlib.org/stable/gallery/color/named_colors.html)
-#
-# return:
-# returns matplotlib.figure.Figure Object 
-'''
-def plot_err(x: np.ndarray,y: np.ndarray,err: np.ndarray, head="", xlbl="", ylbl="", cl=""):
-    fig, ax1 = plt.subplots(1)
-
-    ax1.errorbar(x,y,err, color=cl)
-    ax1.grid()
-    ax1.set(xlabel=str(xlbl), ylabel=str(ylbl))
-    fig.suptitle(str(head))
-    ax1.scatter(x,y,color=cl)
-
-    return fig
-
 
 '''
-# creates a plot with 'm'-Graphs
+# creates a errorbar-plot with 'n'-Graphs
 # 
 # args:
 # x,y  - np.ndarray (n,m) : n lists with m-datapoints
@@ -37,24 +12,52 @@ def plot_err(x: np.ndarray,y: np.ndarray,err: np.ndarray, head="", xlbl="", ylbl
 # xlbl : Label of x-Axis
 # ylbl : label of y-Axis
 # legend: names of every single graph
-# cl: color of graph (see on https://matplotlib.org/stable/gallery/color/named_colors.html)
+# cl: color of graphs (see on https://matplotlib.org/stable/gallery/color/named_colors.html)
 #
 # return:
 # returns matplotlib.figure.Figure Object 
 '''
-def plot_err_mul(x: np.ndarray,y: np.ndarray,err: np.ndarray, head="", xlbl="", ylbl="", legend=[], cl=[]):
+def plot_err(x: np.ndarray,y: np.ndarray,err: np.ndarray, head="", xlbl="", ylbl="", legend=[], cl=[]):
     fig, ax1 = plt.subplots(1)
     for i in range(x.shape[0]):
         if len(legend) <= x.shape[0]:
             for _ in range(x.shape[0] - len(legend)):
                 legend.append("")
         ax1.errorbar(x[i, :], y[i, :], err[i, :], label=legend[i], c=cl[i])
-        ax1.scatter(x[i,:], y[i,:], c=cl[i])
+        ax1.scatter(x[i, :], y[i, :], c=cl[i])
     ax1.grid()
     ax1.set(xlabel=str(xlbl), ylabel=str(ylbl))
     ax1.legend()
     fig.suptitle(str(head))
     return fig
+
+'''
+# plots multiple graphs (n "lines")
+# 
+# args:
+# x,y - np.ndarray (n,m): n lists with m datapoints
+# xlbl, ylbl : label of x and y-axis
+# legend : short description of every graph
+# color : color for every graph
+#
+# returns:
+# plt.Figure object
+'''
+def plot_lin(x: np.ndrarray, y: np.ndarray, head = "", xlbl = "", ylbl = "", legend = [], color=[]):
+    fig, ax1 = plt.subplots(1)
+
+    for i in range(x.shape[0]):
+        if len(legend)<=x.shape[0]:
+            for _ in range(x.shape[0] - len(legend)):
+                legend.append("")
+        ax1.plot(x[i, :], y[i, :], label=legend[i], c=color[i])
+    ax1.plot(x,y,c=color)
+    ax1.scatter(x,y,c=color)
+    ax1.grid()
+    ax1.set(xlabel=xlbl, ylabel=ylbl)
+    return fig
+
+
 '''
 # saves the plot as an image
 #
