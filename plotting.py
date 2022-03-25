26f6ddfa988d62e1c9ffb08774f95b343d8d1da0
Adrian <adrian.hammermeister55@gmail.com>
2022-03-25 17:26:05 +0100

    fixed plotting 1D-Array

--- plotting.py
+++ plotting.py
@@ -43,16 +43,26 @@ def plot_err(x: np.ndarray,y: np.ndarray,err: np.ndarray, head="", xlbl="", ylbl
 # returns:
 # plt.Figure object
 '''
-def plot_lin(x: np.ndrarray, y: np.ndarray, head = "", xlbl = "", ylbl = "", legend = [], color=[]):
+def plot_lin(x: np.ndarray, y: np.ndarray, head = "", xlbl = "", ylbl = "", legend = [], color=[]):
     fig, ax1 = plt.subplots(1)
 
-    for i in range(x.shape[0]):
-        if len(legend)<=x.shape[0]:
-            for _ in range(x.shape[0] - len(legend)):
-                legend.append("")
-        ax1.plot(x[i, :], y[i, :], label=legend[i], c=color[i])
-    ax1.plot(x,y,c=color)
-    ax1.scatter(x,y,c=color)
+    if len(x.shape) == 2:
+        for i in range(x.shape[0]):
+            if len(legend)<=x.shape[0]:
+                for _ in range(x.shape[0] - len(legend)):
+                    legend.append("")
+            if len(color) <= x.shape[0]:
+                for _ in range(x.shape[0]- len(color)):
+                    color.append(None)
+            ax1.plot(x[i, :], y[i, :], label=legend[i], c=color[i])
+            ax1.scatter(x[i, :], y[i, :], c=color[i])
+    else:
+        if len(legend) == 0:
+            legend.append("")
+        if len(color) == 0:
+            color.append(None)
+        ax1.plot(x, y, c=color[0])
+        ax1.scatter(x,y,c=color[0])
     ax1.grid()
     ax1.set(xlabel=xlbl, ylabel=ylbl)
     return fig
